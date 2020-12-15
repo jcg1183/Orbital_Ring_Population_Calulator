d3c850939eca933456099a7e1931ae56b21e6d6e
Joshua Greene <jbookpro@jbookpro.attlocal.net>
2020-12-15 15:45:20 -0600

    Update Comments

--- code/pathgen.py
+++ code/pathgen.py
@@ -8,17 +8,26 @@ from geographiclib.geodesic import Geodesic
 import settings
 from objs import Point, Ring
 
+
 #set geodesic reference system
 geod = Geodesic.WGS84
 
-#check for missing parameters after ring creation
-#fill in missing parameters as necessary
+
+# ***************************************************************
+# Function:         ring_check
+# Variables/input:  object type ring
+# Output:           writes points to ring object
+# Usage/Purpose:    Function checks parameters of the ring object
+#					to detect if a point and azimuth was given
+#					or two pointers were given.  Prepares ring 
+#					for use.
+# ***************************************************************
 def ring_check(ring):
 	start_lat = ring.start_point.lat
 	start_lon = ring.start_point.lon
 	azim = ring.azim
 
-	#start point and end point given, calculate azimuth
+	# start point and end point given, calculate azimuth
 	if azim == -99:
 		end_lat = ring.end_point.lat
 		end_lon = ring.end_point.lon
@@ -27,25 +36,33 @@ def ring_check(ring):
 		g = temp.Position(0, Geodesic.STANDARD | Geodesic.LONG_UNROLL)
 		ring.azim = g['azi1']
 	
-	#start point and azimuth given, calculate end point
+	# start point and azimuth given, calculate end point
 	else:
 		length = ring.resolution * 10
 		
-		#setup for line, not ring
+		# setup for line, not ring
 		if settings.line == 1:
 			length = settings.earth_circ
 		
-		#calculate second lat/lon for map
+		# calculate second lat/lon for map
 		gc = geod.Direct(start_lat, start_lon, azim, length)
 
 		ring.end_point = Point(gc['lat2'], gc['lon2'])
 
-	#calculate length for line between two lat/lons
+	# calculate length for line between two lat/lons
 	if settings.line == 2:
 		g = geod.Inverse(start_lat, start_lon, end_lat, end_lon)
 		settings.earth_circ = int(g['s12']) 
 
 
+# ***************************************************************
+# Function:         generate_gc
+# Variables/input:  object type ring
+# Output:           none
+# Usage/Purpose:    Function iteratively generates points along
+#					the great circle specified then calls
+#					generate_perpendicular for each point.
+# ***************************************************************
 def generate_gc(ring):	
 	start_time = time.time()
 
@@ -56,6 +73,7 @@ def generate_gc(ring):
 	print("Lat %.4f Lon %.4f Azim %.4f - Generating Path" % 
 		 (ring.start_point.lat, ring.start_point.lon, ring.azim))
 
+	# iterate along great circle path by length ring.resolution
 	for i in range(0, settings.earth_circ + 1, ring.resolution):
 		gc = geod.Direct(start_lat, start_lon, azim, i)
 		
@@ -68,10 +86,19 @@ def generate_gc(ring):
 	ring.path_calculation_time = end_time - start_time
 
 
+# ***************************************************************
+# Function:         generate_perpendicular
+# Variables/input:  object type ring
+# Output:           writes points to ring object
+# Usage/Purpose:    Function takes a point along the great circle
+#					and generates a list of perpendicular points
+#					which are saved.
+# ***************************************************************
 def generate_perpendicular(ring, point, azim1, count):
 	lat = point[0]
 	lon = point[1]
 
+	# adjust azimuth to meet library usage requirements
 	azim_left = azim1 - 90
 	if azim_left < 0:
 		azim_left = 360 + azim_left
@@ -80,6 +107,7 @@ def generate_perpendicular(ring, point, azim1, count):
 	if azim_right > 360:
 		azim_right = azim_right - 360
 	
+	# determine number of points to calculate
 	n = int(math.ceil(ring.width / ring.resolution))
 	
 	left_point = ''
@@ -87,7 +115,7 @@ def generate_perpendicular(ring, point, azim1, count):
 
 	perp_points = []
 
-	#Calculate left branch of the perpendicular
+	# Calculate left branch of the perpendicular
 	for i in range(n, 0, -1):
 		left_path = geod.Direct(lat, lon, azim_left, min(ring.resolution * i, ring.width))
 
@@ -96,11 +124,11 @@ def generate_perpendicular(ring, point, azim1, count):
 		if i == n:
 			left_point = Point(left_path['lat2'], checked_lon)
 
-		#Add calculated point to list of points
+		# Add calculated point to list of points
 		perp_points.append(Point(left_path['lat2'], checked_lon))
 		ring.num_points += 1
 
-	#Calculate right branch of the perpendicular
+	# Calculate right branch of the perpendicular
 	for i in range(0, n+1):
 		right_path = geod.Direct(lat, lon, azim_right, min(ring.resolution * i, ring.width))
 
@@ -116,12 +144,19 @@ def generate_perpendicular(ring, point, azim1, count):
 
 	ring.points.append([point, perp_points])
 
-	#Add perpendicular line to list of perps
+	# Add perpendicular line to list of perps in ring object
 	if settings.show_width == 1 and count % ring.interval == 0:
 		ring.perpendiculars.append([left_point, right_point])
 		ring.num_perpendiculars += 1
 
 
+# ***************************************************************
+# Function:         check_lon
+# Variables/input:  float longitude
+# Output:           float lon
+# Usage/Purpose:    Function converts longitude to the convention
+#					of libraries used.
+# ***************************************************************
 def check_lon(lon):	
 	if lon > 180:
 		lon = lon - 360
